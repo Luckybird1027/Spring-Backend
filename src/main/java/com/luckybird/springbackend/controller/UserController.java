f093bc6d60c2927c1a2f422e1aa459649ba173cd
LuckyBrid <1342979416@qq.com>
2024-08-05 17:23:23 +0800

    reconst：调整仓库追踪文件，添加VO，将其作为Controller的返回结果载体，并修改项目包名为springbackend

--- src/main/java/com/luckybird/springbackend/controller/UserController.java
+++ src/main/java/com/luckybird/springbackend/controller/UserController.java
@@ -37,6 +37,7 @@ public class UserController {
         return userLoginVo;
     }
 
+
     @PostMapping("/register")
     public UserRegistrationVo register(@RequestBody @Valid UserRegistrationDto userRegistrationDto) {
         User user = userService.register(userRegistrationDto);
