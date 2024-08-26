67fa97f0709477ba8aef6c92b49f0ca1403a5281
LuckyBrid <1342979416@qq.com>
2024-08-26 11:54:38 +0800

    feat：配置分页拦截器以实现分页功能

---
+++ src/main/java/com/luckybird/springbackend/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.luckybird.springbackend.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 新云鸟
+ */
+@Configuration
+@MapperScan("com.luckybird.springbackend.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
