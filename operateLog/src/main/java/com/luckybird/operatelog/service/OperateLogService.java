28d6ca17a07d4f90d3d2eebda7bb0d523979fe34
LuckyBrid <1342979416@qq.com>
2024-09-19 20:41:37 +0800

    adjust：添加注释，添加自动配置

--- operateLog/src/main/java/com/luckybird/operatelog/service/OperateLogService.java
+++ operateLog/src/main/java/com/luckybird/operatelog/service/OperateLogService.java
@@ -13,11 +13,36 @@ import java.util.Set;
  * @author 新云鸟
  */
 public interface OperateLogService {
+
+    /**
+     * 获取操作日志
+     *
+     * @param id 日志ID
+     * @return 操作日志信息
+     */
     OperateLogVO get(Long id);
 
+    /**
+     * 批量获取操作日志
+     * @param ids 日志ID集合
+     * @return 操作日志信息集合
+     */
     List<OperateLogVO> batchGet(Set<Long> ids);
 
+    /**
+     * 获取操作日志列表
+     * @param req 查询请求
+     * @return 操作日志列表
+     */
     List<OperateLogVO> list(OperateLogQueryReq req);
 
+    /**
+     * 分页获取操作日志列表
+     * @param req 查询请求
+     * @param current 页码
+     * @param pageSize 页大小
+     * @param searchCount 是否搜索总数
+     * @return 分页结果
+     */
     PageResult<OperateLogVO> page(OperateLogQueryReq req, Long current, Long pageSize, boolean searchCount);
 }
